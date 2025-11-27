0d976d1d64aa89002ea914ec1aae90336d9c3cb6
maxie <infomaxieandme@gmail.com>
2025-11-27 04:44:07 +0100

    I wrote the terraform files

---
+++ main.tf
@@ -0,0 +1,32 @@
+module "vpc" {
+  source                = "./modules/vpc"
+  env                   = var.env
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  azs                   = var.azs
+  eip_id                = var.eip_id
+}
+
+module "iam" {
+  source = "./modules/iam"
+  env    = var.env
+}
+
+module "eks" {
+  source               = "./modules/eks"
+  env                  = var.env
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  cluster_role_arn     = module.iam.eks_cluster_role_arn
+  node_role_arn        = module.iam.eks_node_role_arn
+  node_desired_size    = var.node_desired_size
+  node_min_size        = var.node_min_size
+  node_max_size        = var.node_max_size
+  instance_types       = var.instance_types
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+  env    = var.env
+  repos  = var.ecr_repos
+}
